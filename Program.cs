e2848d4c6faf536e46bf17192391f5db61bdb355
Pls8 <storyingmasod@gmail.com>
2025-10-10 10:13:45 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number = [99999, 88888, 77777];
+            string[] Name = ["N1", "N2","N3"];
+
+            Console.WriteLine($"Customer Name {Name[0]} ," + $"The Number {Number[0]}");
+
+            int[] stdGrade = new int[3]; // array size 3 index
+            stdGrade[0] = 9; // assign by INDEX
+
+            //Condtion 
+            string perfect = (stdGrade[0] >= 0) ? "Number in system" : "Number not found in system";
+
+            
+
+
+        }
+    }
+}
