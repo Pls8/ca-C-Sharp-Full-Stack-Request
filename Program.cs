9fef2692c6d4f46da6ee9fdeba218c3866814446
Pls8 <storyingmasod@gmail.com>
2025-10-10 10:41:46 +0400

    add ignore

--- Program.cs
+++ Program.cs
@@ -10,10 +10,10 @@
             Console.WriteLine($"Customer Name {Name[0]} ," + $"The Number {Number[0]}");
 
             int[] stdGrade = new int[3]; // array size 3 index
-            stdGrade[0] = 9; // assign by INDEX
+            Number[0] = 55555; // assign by INDEX
 
             //Condtion 
-            string perfect = (stdGrade[0] >= 0) ? "Number in system" : "Number not found in system";
+            string perfect = (Number[0] >= 0) ? "Number in system" : "Number not found in system";
 
             
 
